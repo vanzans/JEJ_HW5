55a055f8d03266d098728d1bc9cacccb3b1cca48
Joshua Johnson <joshuaj808@gmail.com>
2015-12-17 18:01:38 -0500

    second commit HW5

---
+++ cheats.js
@@ -0,0 +1,98 @@
+// Using COMMENTS is a good way to explain what you are
+// working on and leave notes for yourself or others
+// they are also helpful when DEBUGGING
+
+// VARIABLES are nameable, changeable JS elements
+// they act as containers for an incoming value
+
+// you can TEST JS code by (1) showing your results in
+// the console:
+
+var num1 = 100;
+var num2 = 200;
+var result = num1 + num2;
+console.log(result);
+
+
+// or (2) by using an alert
+
+var firstName = "Johnny_"
+var lastName = "TwoShoes"
+var result = firstName + lastName;
+alert(result);
+
+// or (3) by looging to the HTML
+
+function switcheroo(){
+document.getElementById("bowie").innerHTML = "CHANGES!"
+}
+
+// see jsbasics.html for DATA TYPES
+
+// ARRAYS are collections of data types:
+// ["Persons, Places, Things"]
+// ["5, 10, 50, 2500"]
+// ["2, 4, 6, 8, who, do, we, appreciate"]
+
+// ARRAYS can also be stored in variables:
+// var dogNames = ["spot, fido, smalltalk"]
+
+// LOGIC is the control flow of the program.
+// It's also a river, apparently.
+
+
+// TESTING("1, 2, 3")
+// *hint: for strings use 3 = signs for testing equality,
+// use !== for inequality
+// <, >, <=, >= are also valid comparison operators
+
+// Simple Math Ex:
+// 5 = 5
+// true
+
+// 5 = 5
+// false
+
+// "banana" === "bananas"
+// false
+
+
+// TESTING with if, else
+
+if(10>20){
+	alert("Your Basic Math Skills Need Immediate Attention!")
+}
+
+if (10<20){
+	alert("Your Basic Math Skills Are Very Handsome Indeed!")
+}
+
+
+if(10>20){
+	console.log("Your Basic Math Skills Are Very Handsome Indeed!")
+} else {
+	console.log("Your Basic Math Skills Need Immediate Attention!")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
